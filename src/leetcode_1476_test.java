b891ba9cbe17e4038cec6071e5ce2c601e0ec64c
tom_glb <goodMorning_glb@atguigu.com>
2021-05-04 22:09:59 +0800

    leetcode_20210324

---
+++ src/leetcode_1476_test.java
@@ -0,0 +1,8 @@
+public class leetcode_1476_test {
+
+    public static void main(String[] args) {
+        int[][] arr = {};
+        leetcode_1476 leetcode_1476 = new leetcode_1476(arr);
+    }
+
+}
